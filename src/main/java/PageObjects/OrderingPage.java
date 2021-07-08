35a5cbef8478ffe3674e883ed961e5c777c071f6
Wojtek Puchalski <wpuchalski@o2.pl>
2021-07-08 17:01:47 +0200

    fillingAddressFormStepOneOrderingPageTestNegativeWays

--- src/main/java/PageObjects/OrderingPage.java
+++ src/main/java/PageObjects/OrderingPage.java
@@ -8,6 +8,17 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 public class OrderingPage extends BasePage{
 
     private final By headingStepOneOrderingPageSelector = new By.ByCssSelector("h1");
+    private final By nameInputInStepOneFormOrderingPageSelector = new By.ByCssSelector("#shipping_details_name");
+    private final By addressInputInStepOneFormOrderingPageSelector = new By.ByCssSelector("#shipping_details_street");
+    private final By cityInputInStepOneFormOrderingPageSelector = new By.ByCssSelector("#shipping_details_city");
+    private final By codeInputInStepOneFormOrderingPageSelector = new By.ByCssSelector("#shipping_details_postalCode");
+    private final By telInputInStepOneFormOrderingPageSelector = new By.ByCssSelector("#shipping_details_phone");
+    private final By emailInputInStepOneFormOrderingPageSelector = new By.ByCssSelector("#shipping_details_email");
+    private final By commentsInputInStepOneFormOrderingPageSelector = new By.ByCssSelector("#shipping_details_notes");
+    private final By invoiceOptionInStepOneFormOrderingPageSelector = new By.ByCssSelector("#shipping_details_hasInvoice");
+    private final By nipInputInStepOneFormOrderingPageSelector = new By.ByCssSelector("#shipping_details_taxId");
+    private final By continueButtonInStepOneFormOrderingPageSelector = new By.ByCssSelector("#shipping_details_save");
+
 
     public OrderingPage(WebDriver driver) {
         super(driver);
@@ -43,6 +54,124 @@ public class OrderingPage extends BasePage{
         return driver.findElement(headingStepOneOrderingPageSelector).getText();
 
     }
+
+    public OrderingPage fillNameInStepOneForm(String name){
+
+        driver.findElement(nameInputInStepOneFormOrderingPageSelector).sendKeys(name);
+        return this;
+    }
+
+    public OrderingPage clearNameInStepOneForm(){
+
+        driver.findElement(nameInputInStepOneFormOrderingPageSelector).clear();
+        return this;
+    }
+
+
+    public OrderingPage fillAddressInStepOneForm(String address){
+
+        driver.findElement(addressInputInStepOneFormOrderingPageSelector).sendKeys(address);
+        return this;
+    }
+
+    public OrderingPage clearAddressInStepOneForm(){
+
+        driver.findElement(addressInputInStepOneFormOrderingPageSelector).clear();
+        return this;
+    }
+
+    public OrderingPage fillCodeInStepOneForm(String code){
+
+        driver.findElement(codeInputInStepOneFormOrderingPageSelector).sendKeys(code);
+        return this;
+    }
+
+    public OrderingPage clearCodeInStepOneForm(){
+
+        driver.findElement(codeInputInStepOneFormOrderingPageSelector).clear();
+        return this;
+    }
+
+    public OrderingPage fillCityInStepOneForm(String city){
+
+        driver.findElement(cityInputInStepOneFormOrderingPageSelector).sendKeys(city);
+        return this;
+    }
+
+    public OrderingPage clearCityInStepOneForm(){
+
+        driver.findElement(cityInputInStepOneFormOrderingPageSelector).clear();
+        return this;
+    }
+
+    public OrderingPage fillTelInStepOneForm(String tel){
+
+        driver.findElement(telInputInStepOneFormOrderingPageSelector).sendKeys(tel);
+        return this;
+    }
+
+    public OrderingPage clearTelInStepOneForm(){
+
+        driver.findElement(telInputInStepOneFormOrderingPageSelector).clear();
+        return this;
+    }
+
+    public OrderingPage fillEmailInStepOneForm(String email){
+
+        driver.findElement(emailInputInStepOneFormOrderingPageSelector).sendKeys(email);
+        return this;
+    }
+
+    public OrderingPage clearEmailInStepOneForm(){
+
+        driver.findElement(emailInputInStepOneFormOrderingPageSelector).clear();
+        return this;
+    }
+
+    public OrderingPage fillCommentsInStepOneForm(String comments){
+
+        driver.findElement(commentsInputInStepOneFormOrderingPageSelector).sendKeys(comments);
+        return this;
+    }
+
+    public OrderingPage clearCommentsInStepOneForm(){
+
+        driver.findElement(commentsInputInStepOneFormOrderingPageSelector).clear();
+        return this;
+    }
+
+    public OrderingPage isInvoiceNeededOptionSteoOneForm(Boolean invoice){
+
+        driver.findElement(invoiceOptionInStepOneFormOrderingPageSelector).click();
+        return this;
+    }
+
+    public OrderingPage clearIsInvoiceNeededOptionSteoOneForm(){
+
+        driver.findElement(invoiceOptionInStepOneFormOrderingPageSelector).clear();
+        return this;
+    }
+
+    public OrderingPage fillNipInStepOneForm(String nip){
+
+        driver.findElement(nipInputInStepOneFormOrderingPageSelector).sendKeys(nip);
+        return this;
+    }
+
+    public OrderingPage clearNipInStepOneForm(){
+
+        driver.findElement(nipInputInStepOneFormOrderingPageSelector).clear();
+        return this;
+    }
+
+
+    public OrderingPage saveAndContinueOrderingPage(){
+
+        driver.findElement(continueButtonInStepOneFormOrderingPageSelector).click();
+        return this;
+    }
+
+
 
 
 }
--- src/test/java/POMTests/OrderingTests.java
+++ src/test/java/POMTests/OrderingTests.java
@@ -128,4 +128,131 @@ public class OrderingTests extends BaseTests {
         assertEquals(heading, headingStepOneOrderingPage, "Naglowek w pierwszym kroku zamowienia nie zgadza sie");
 
     }
+
+    @Test
+    public void fillingAddressFormStepOneOrderingPageTestNegativeWays(){
+
+        String name = "Maria Biedronka";
+        String address = "Kowalewskiego 3/4";
+        String city = "Sopot";
+        String code = "83-123";
+        String email = "maria1@o2.pl";
+        String email2 = "maria1o2.pl";
+        String email3 = "@maria1o2.pl";
+        String email4 = "maria&;#@o2.pl";
+
+        String tel = "600123098";
+        String comments = "Prosze o formularz";
+        Boolean invoice = true;
+        String nip = "7333300440";
+
+        String category = categories[4];
+        String symbol = "sa32";
+        String message1 = "Wypełnij to pole.";
+        String message2 = "Uwzględnij znak „@” w adresie e-mail. W adresie „" + email2 + "” brakuje znaku „@”.";
+        String message3 = "Podaj część przed znakiem „@”. Adres „" + email3 + "” jest niepełny.";
+        String message4 = "Część przed znakiem „@” nie może zawierać symbolu „;”.";
+
+        String nameAlertMessage = "";
+        String addressAlertMessage = "";
+        String codeAlertMessage = "";
+        String telAlertMessage = "";
+        String emailAlertMessage = "";
+        String cityAlertMessage = "";
+        String commentsAlertMessage = "";
+        String nipAlertMessage = "";
+
+
+
+        //case 1 :: field name is empty
+
+        MainCategoryPage mainCategoryPage = new MainCategoryPage(driver);
+        mainCategoryPage.acceptCookie();
+        OrderingPage orderingPage = mainCategoryPage.viewCategoryByName(category).addToCartByCategoryPage(symbol).
+                setRealizationType().setDeliveryType().saveAndOrderProductsFromCart().fillAddressInStepOneForm(address).fillCityInStepOneForm(city).
+                fillCodeInStepOneForm(code).fillEmailInStepOneForm(email).fillTelInStepOneForm(tel).
+                isInvoiceNeededOptionSteoOneForm(invoice).fillNipInStepOneForm(nip).saveAndContinueOrderingPage();
+
+        JavascriptExecutor jse = (JavascriptExecutor) driver;
+        nameAlertMessage = (String)jse.executeScript(
+                "const name = document.querySelector(\"#shipping_details_name\");"
+                        + "message = name.validationMessage; return message;");
+
+        assertEquals(message1, nameAlertMessage, "Komunikat jest bledny");
+
+
+        //case 2 :: field address is empty
+
+        orderingPage = orderingPage.clearAddressInStepOneForm().fillNameInStepOneForm(name).saveAndContinueOrderingPage();
+
+        addressAlertMessage = (String)jse.executeScript(
+                "const address = document.querySelector(\"#shipping_details_street\");"
+                        + "message = address.validationMessage; return message;");
+
+        assertEquals(message1, addressAlertMessage, "Komunikat jest bledny");
+
+        //case 3 :: field code is empty
+
+        orderingPage = orderingPage.clearCodeInStepOneForm().fillAddressInStepOneForm(address).saveAndContinueOrderingPage();
+
+        codeAlertMessage = (String)jse.executeScript(
+                "const code = document.querySelector(\"#shipping_details_postalCode\");"
+                        + "message = code.validationMessage; return message;");
+
+        assertEquals(message1, codeAlertMessage, "Komunikat jest bledny");
+
+        //case 4 :: field email is empty
+
+        orderingPage = orderingPage.clearEmailInStepOneForm().fillCodeInStepOneForm(code).saveAndContinueOrderingPage();
+
+        emailAlertMessage = (String)jse.executeScript(
+                "const email = document.querySelector(\"#shipping_details_email\");"
+                        + "message = email.validationMessage; return message;");
+
+        assertEquals(message1, emailAlertMessage, "Komunikat jest bledny");
+
+        //case 5 :: field tel is empty
+
+        orderingPage = orderingPage.clearTelInStepOneForm().fillEmailInStepOneForm(email).saveAndContinueOrderingPage();
+
+        telAlertMessage = (String)jse.executeScript(
+                "const tel = document.querySelector(\"#shipping_details_phone\");"
+                        + "message = tel.validationMessage; return message;");
+
+        assertEquals(message1, telAlertMessage, "Komunikat jest bledny");
+
+        //case 6 :: field email has not got '@'
+
+        orderingPage = orderingPage.clearEmailInStepOneForm().fillTelInStepOneForm(tel).fillEmailInStepOneForm(email2).saveAndContinueOrderingPage();
+
+        emailAlertMessage = (String)jse.executeScript(
+                "const email = document.querySelector(\"#shipping_details_email\");"
+                        + "message = email.validationMessage; return message;");
+        System.out.println(emailAlertMessage);
+
+        assertEquals(message2, emailAlertMessage, "Komunikat jest bledny");
+
+        //case 7 :: field email has got nothing before '@'
+
+        orderingPage = orderingPage.clearEmailInStepOneForm().fillEmailInStepOneForm(email3).saveAndContinueOrderingPage();
+
+        emailAlertMessage = (String)jse.executeScript(
+                "const email = document.querySelector(\"#shipping_details_email\");"
+                        + "message = email.validationMessage; return message;");
+        System.out.println(emailAlertMessage);
+
+        assertEquals(message3, emailAlertMessage, "Komunikat jest bledny");
+
+        //case 8 :: field email has got wrong sings
+
+        orderingPage = orderingPage.clearEmailInStepOneForm().fillEmailInStepOneForm(email4).saveAndContinueOrderingPage();
+
+        emailAlertMessage = (String)jse.executeScript(
+                "const email = document.querySelector(\"#shipping_details_email\");"
+                        + "message = email.validationMessage; return message;");
+        System.out.println(emailAlertMessage);
+
+        assertEquals(message4, emailAlertMessage, "Komunikat jest bledny");
+
+    }
 }
